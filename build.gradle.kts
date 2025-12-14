356734942ed75cad48239430167f5cff911654a4
amineharbaoui <amineharbaoui94@gmail.com>
2025-12-14 14:56:19 +0100

    feat: Add module dependency graph plugin
    Add the `module-dependency-graph` gradle plugin and update the root `build.gradle.kts` to apply it. Configure the version in `libs.versions.toml`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,11 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
-    id(libs.plugins.google.services.get().pluginId) version libs.versions.googleServices.get() apply false
+    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.dependency.analysis)
+    alias(libs.plugins.module.dependency.graph)
 }
 
 subprojects {
