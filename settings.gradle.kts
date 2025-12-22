3d0769c1c5ba6a3a9bb108b7e2bff909e927a8ab
amineharbaoui <amineharbaoui94@gmail.com>
2025-12-23 00:25:35 +0100

    build: initialize project configuration and version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,6 +20,29 @@ dependencyResolutionManagement {
     }
 }
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 rootProject.name = "LazyPizza"
 include(":app")
- 
+
+include(":core:designsystem")
+include(":core:ui")
+include(":core:model")
+
+include(":features:menu:data")
+include(":features:menu:domain")
+include(":features:menu:ui-home")
+include(":features:menu:ui-pizza-detail")
+
+include(":features:cart:data")
+include(":features:cart:domain")
+include(":features:cart:ui-cart")
+
+include(":features:auth:data")
+include(":features:auth:ui-login")
+include(":features:auth:domain")
+
+include(":features:order:domain")
+include(":features:order:data")
+include(":features:order:ui-checkout")
+include(":features:order:ui-past-orders")
