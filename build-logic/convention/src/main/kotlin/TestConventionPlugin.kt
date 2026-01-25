163924fd2e8d418a9961a93da815a6c7d97107c1
amineharbaoui <amineharbaoui94@gmail.com>
2026-01-25 15:53:31 +0100

    refactor(:build-logic): update to Java 17 and Kotlin compiler options
    - Set `sourceCompatibility` and `targetCompatibility` to Java 17 in `KotlinModuleConventionPlugin`
    - Update `jvmTarget` to JVM 17 in Kotlin compiler options
    - Add `-Xannotation-default-target=param-proprety` to `freeCompilerArgs`
    - Refactor `TestConventionPlugin` to use Kotlin's `withType` extension for improved readability

--- build-logic/convention/src/main/kotlin/TestConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/TestConventionPlugin.kt
@@ -2,11 +2,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
 
 class TestConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            tasks.withType(Test::class.java).configureEach {
+            tasks.withType<Test>().configureEach {
                 useJUnitPlatform()
             }
 
