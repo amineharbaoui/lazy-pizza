8dca8db24fb08da421af63717a91af3923fd1995
amineharbaoui <amineharbaoui94@gmail.com>
2026-01-31 15:53:31 +0100

    feat(:core:testing): include core:testing module in project configuration

---
+++ core/testing/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.custom.android.module)
+}
+
+android {
+    namespace = "com.example.testing"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.junit.jupiter)
+    implementation(libs.kotlinx.coroutines.test)
+}
